5fb6d048d1101de74b0b537dd000028b08c0ee39
angular-cli <angular-cli@angular.io>
2016-05-08 21:51:58 -0500

    chore: initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ src/app/index.ts
@@ -0,0 +1,2 @@
+export {environment} from './environment';
+export {Ng2CliAppComponent} from './ng2-cli.component';
---
+++ src/app/ng2-cli.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'ng2-cli-app',
+  templateUrl: 'ng2-cli.component.html',
+  styleUrls: ['ng2-cli.component.css']
+})
+export class Ng2CliAppComponent {
+  title = 'ng2-cli works!';
+}
